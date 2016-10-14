a08657dc9339e4ca7b7861fa61dcf15329ef8cc6
An Vu <anvu@Ans-MacBook-Pro.local>
2016-10-14 02:32:16 -0500

    updated classes
    :

--- StockSeer/src/main/java/gui/ChooseStockPane.java
+++ StockSeer/src/main/java/gui/ChooseStockPane.java
@@ -13,9 +13,11 @@ import javax.swing.Box;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
 
+import model.AvailableStockModel;
+
 public class ChooseStockPane extends JPanel {
-	DefaultListModel availableUsers;
-	DefaultListModel addedUsers;
+	AvailableStockModel availStockModel;
+	DefaultListModel addedStockModel;
 
 	/**
 	 * Create the panel.
@@ -28,25 +30,25 @@ public class ChooseStockPane extends JPanel {
 		gridBagLayout.rowWeights = new double[] { 1.0, Double.MIN_VALUE };
 		setLayout(gridBagLayout);
 
-		JScrollPane scrollPane = new JScrollPane();
-		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
-		gbc_scrollPane.insets = new Insets(0, 0, 0, 5);
-		gbc_scrollPane.fill = GridBagConstraints.BOTH;
-		gbc_scrollPane.gridx = 0;
-		gbc_scrollPane.gridy = 0;
-		add(scrollPane, gbc_scrollPane);
+		JScrollPane scrollPaneAvailable = new JScrollPane();
+		GridBagConstraints gbc_scrollPaneAvailable = new GridBagConstraints();
+		gbc_scrollPaneAvailable.insets = new Insets(0, 0, 0, 5);
+		gbc_scrollPaneAvailable.fill = GridBagConstraints.BOTH;
+		gbc_scrollPaneAvailable.gridx = 0;
+		gbc_scrollPaneAvailable.gridy = 0;
+		add(scrollPaneAvailable, gbc_scrollPaneAvailable);
 
-		JList listLeft = new JList();
-		scrollPane.setViewportView(listLeft);
-		availableUsers = new DefaultListModel();
+		JList availableStocks = new JList();
+		scrollPaneAvailable.setViewportView(availableStocks);
+		availStockModel = new AvailableStockModel();
 		String[] demoAvailUesr = { "An", "John" };
 		for (String temp : demoAvailUesr) {
-			availableUsers.addElement(temp);
+			availStockModel.addElement(temp);
 		}
-		listLeft.setModel(availableUsers);
+		availableStocks.setModel(availStockModel);
 
 		Component horizontalGlue = Box.createHorizontalGlue();
-		scrollPane.setColumnHeaderView(horizontalGlue);
+		scrollPaneAvailable.setColumnHeaderView(horizontalGlue);
 
 		JPanel panel_1 = new JPanel();
 		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
@@ -60,32 +62,32 @@ public class ChooseStockPane extends JPanel {
 		Component verticalGlue = Box.createVerticalGlue();
 		panel_1.add(verticalGlue);
 
-		JButton btnNewButton = new JButton(">>");
-		btnNewButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-		panel_1.add(btnNewButton);
+		JButton btnAdd = new JButton(">>");
+		btnAdd.setAlignmentX(Component.CENTER_ALIGNMENT);
+		panel_1.add(btnAdd);
 
-		JButton btnNewButton_1 = new JButton("<<");
-		btnNewButton_1.setAlignmentX(Component.CENTER_ALIGNMENT);
-		panel_1.add(btnNewButton_1);
+		JButton btnRemove = new JButton("<<");
+		btnRemove.setAlignmentX(Component.CENTER_ALIGNMENT);
+		panel_1.add(btnRemove);
 
 		Component verticalGlue_1 = Box.createVerticalGlue();
 		panel_1.add(verticalGlue_1);
 
-		JScrollPane scrollPane_1 = new JScrollPane();
-		GridBagConstraints gbc_scrollPane_1 = new GridBagConstraints();
-		gbc_scrollPane_1.fill = GridBagConstraints.BOTH;
-		gbc_scrollPane_1.gridx = 2;
-		gbc_scrollPane_1.gridy = 0;
-		add(scrollPane_1, gbc_scrollPane_1);
+		JScrollPane scrollPaneAdded = new JScrollPane();
+		GridBagConstraints gbc_scrollPaneAdded = new GridBagConstraints();
+		gbc_scrollPaneAdded.fill = GridBagConstraints.BOTH;
+		gbc_scrollPaneAdded.gridx = 2;
+		gbc_scrollPaneAdded.gridy = 0;
+		add(scrollPaneAdded, gbc_scrollPaneAdded);
 
-		JList listRight = new JList();
-		listRight.setSize(listLeft.getSize());
-		scrollPane_1.setViewportView(listRight);
+		JList addedStocks = new JList();
+		addedStocks.setSize(availableStocks.getSize());
+		scrollPaneAdded.setViewportView(addedStocks);
 
-		addedUsers = new DefaultListModel();
+		addedStockModel = new DefaultListModel();
 
 		Component horizontalGlue_1 = Box.createHorizontalGlue();
-		scrollPane_1.setColumnHeaderView(horizontalGlue_1);
+		scrollPaneAdded.setColumnHeaderView(horizontalGlue_1);
 
 	}
 
---
+++ StockSeer/src/main/java/model/AvailableStockModel.java
@@ -0,0 +1,12 @@
+package model;
+
+import javax.swing.DefaultListModel;
+
+/**
+ * provide data for choosing stock pane
+ * @author anvu
+ *
+ */
+public class AvailableStockModel extends DefaultListModel {
+	
+}
