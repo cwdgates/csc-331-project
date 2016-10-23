8a7a875faba430b50d68a85f232393951219087e
An Vu <aqv1321@jagmail.southalabama.edu>
2016-10-23 13:44:55 -0500

    from lab comp

--- StockSeer/src/model/LeagueListModel.java
+++ StockSeer/src/model/LeagueListModel.java
@@ -1,10 +1,12 @@
 package model;
 
+import java.util.ArrayList;
+
 /**
  * list of all leagues in the current games
  * @author AnVu
  *
  */
 public class LeagueListModel {
-
+	ArrayList<LeagueModel> leagueList;
 }
