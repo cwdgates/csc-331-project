49fe2fed32cc4bd821bc796ee10c493406d5b786
an vu <aqv1321@jagmail.southalabama>
2016-10-23 01:43:36 -0500

    created leaguemodel

--- StockSeer/src/model/LeagueModel.java
+++ StockSeer/src/model/LeagueModel.java
@@ -1,5 +1,51 @@
 package model;
 
-public class LeagueModel {
+import java.util.Date;
 
+public class LeagueModel {
+	private String leagueName;
+	private String leagueID;
+	private int maxPlayers;
+	private Date startDate;
+	private Date endDate;
+
+	public String getLeagueName() {
+		return leagueName;
+	}
+
+	public void setLeagueName(String leagueName) {
+		this.leagueName = leagueName;
+	}
+
+	public String getLeagueID() {
+		return leagueID;
+	}
+
+	public void setLeagueID(String leagueID) {
+		this.leagueID = leagueID;
+	}
+
+	public int getMaxPlayers() {
+		return maxPlayers;
+	}
+
+	public void setMaxPlayers(int maxPlayers) {
+		this.maxPlayers = maxPlayers;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
 }
