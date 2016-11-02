26961cd76a3d2314b183e45da729593c6a98a018
an vu <aqv1321@jagmail.southalabama>
2016-11-01 20:43:31 -0500

    update

--- StockSeer/src/ui/RegistrationPane.java
+++ StockSeer/src/ui/RegistrationPane.java
@@ -5,7 +5,6 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.GridBagLayout;
-import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.event.KeyEvent;
