06a2baf14d06f72ffa00626fa9f877c2dfcb57d8
An Vu <aqv1321@jagmail.southalabama.edu>
2016-11-02 00:56:51 -0500

    added another model

---
+++ StockSeer/src/model/AvailableStockModel.java
@@ -0,0 +1,37 @@
+package model;
+
+import javax.swing.ListModel;
+import javax.swing.event.ListDataListener;
+
+public class AvailableStockModel implements ListModel {
+
+	public void addElement(String temp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getElementAt(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addListDataListener(ListDataListener l) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeListDataListener(ListDataListener l) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
