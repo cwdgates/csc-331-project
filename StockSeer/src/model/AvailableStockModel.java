12ec8490a1931bb1b6c73bcf9ddcb0b8610d4e84
An Vu <aqv1321@jagmail.southalabama.edu>
2016-11-04 23:38:52 -0500

    remove some todo

--- StockSeer/src/model/AvailableStockModel.java
+++ StockSeer/src/model/AvailableStockModel.java
@@ -5,33 +5,28 @@ import javax.swing.event.ListDataListener;
 
 public class AvailableStockModel implements ListModel {
 
-	public void addElement(String temp) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void addElement(String temp) {
 
-	@Override
-	public int getSize() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    }
 
-	@Override
-	public Object getElementAt(int index) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public int getSize() {
+        return 0;
+    }
 
-	@Override
-	public void addListDataListener(ListDataListener l) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public Object getElementAt(int index) {
+        return null;
+    }
 
-	@Override
-	public void removeListDataListener(ListDataListener l) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void addListDataListener(ListDataListener l) {
+
+    }
+
+    @Override
+    public void removeListDataListener(ListDataListener l) {
+
+    }
 
 }
