6d82d4117a21fcb37993d780c6f61bf3e8f9faf1
an vu <aqv1321@jagmail.southalabama>
2016-11-25 23:36:37 -0600

    Modified LeagueCreationController. Created separate function -> easier to read

--- StockSeer/src/controller/LeagueCreationController.java
+++ StockSeer/src/controller/LeagueCreationController.java
@@ -37,59 +37,7 @@ public class LeagueCreationController implements ActionListener {
              * when BTN_CREATE button is clicked
              */
             case LeagueCreationPane.BTN_CREATE: {
-                // check name
-                if (createLeaguePane.getLeagueName().length() == 0) {
-                    JOptionPane.showMessageDialog(appView, "Please enter the name of the league.", "",
-                            JOptionPane.WARNING_MESSAGE);
-                } else if (LeagueUtility.hasLeague(createLeaguePane.getLeagueName())) {
-                    JOptionPane.showMessageDialog(appView,
-                            "The name has been used in another league.\nPlease use another name.", "",
-                            JOptionPane.WARNING_MESSAGE);
-                } else {
-                    Date startDate = createLeaguePane.getStartDate();
-                    Date endDate = createLeaguePane.getEndDate();
-                    Date currentDate = new Date();
-                    // check start date
-                    if (startDate == null || startDate.before(currentDate)) {
-                        JOptionPane.showMessageDialog(appView, "Start date is invalid.", "", JOptionPane
-                                .WARNING_MESSAGE);
-                    }
-                    // check end date
-                    else if (endDate == null) {
-                        JOptionPane.showMessageDialog(appView, "End date is invalid.", "", JOptionPane.WARNING_MESSAGE);
-                    }
-                    // make sure end date is after start date
-                    else if (!endDate.after(startDate)) {
-                        JOptionPane.showMessageDialog(appView, "End date is not after start date.", "",
-                                JOptionPane.WARNING_MESSAGE);
-                    } else {
-                        // finally get a chance to create league
-                        int choice = JOptionPane.showConfirmDialog(appView, "Do you want to create this league?", "",
-                                JOptionPane.OK_CANCEL_OPTION);
-                        if (choice == JOptionPane.OK_OPTION) {
-                            String name = createLeaguePane.getLeagueName();
-                            int capacity = createLeaguePane.getCapacity();
-                            Difficulty difficulty = createLeaguePane.getDifficulty();
-                            boolean isCreateLeague = LeagueUtility.createLeague(name, capacity, startDate, endDate,
-                                    difficulty, mainUserModel.getUsername());
-                            
-                            boolean isAssignStock = LeagueUtility.assignStocks(createLeaguePane.getLeagueName(),
-                                    createLeaguePane.getStocks());
-                            
-                            if (isCreateLeague && isAssignStock) {
-                                JOptionPane.showMessageDialog(appView, "The league was successfully created.", "",
-                                        JOptionPane.INFORMATION_MESSAGE);
-                                createLeaguePane.resetFields();
-                                leagueListModel.allLeagues();
-                                appView.viewHome();
-                            } else {
-                                JOptionPane.showMessageDialog(appView, "The league was NOT successfully created.", "",
-                                        JOptionPane.WARNING_MESSAGE);
-                            }
-                        }
-                    }
-                }
-                
+                createLeague();
                 break;
             }
             case LeagueCreationPane.BTN_CANCEL: {
@@ -106,5 +54,60 @@ public class LeagueCreationController implements ActionListener {
             default:
                 break;
         }
+    }
+    
+    private void createLeague() {
+        if (createLeaguePane.getLeagueName().length() == 0) {
+            JOptionPane.showMessageDialog(appView, "Please enter the name of the league.", "",
+                    JOptionPane.WARNING_MESSAGE);
+        } else if (LeagueUtility.hasLeague(createLeaguePane.getLeagueName())) {
+            JOptionPane.showMessageDialog(appView,
+                    "The name has been used in another league.\nPlease use another name.", "",
+                    JOptionPane.WARNING_MESSAGE);
+        } else {
+            Date startDate = createLeaguePane.getStartDate();
+            Date endDate = createLeaguePane.getEndDate();
+            Date currentDate = new Date();
+            // check start date
+            if (startDate == null || startDate.before(currentDate)) {
+                JOptionPane.showMessageDialog(appView, "Start date is invalid.", "", JOptionPane
+                        .WARNING_MESSAGE);
+            }
+            // check end date
+            else if (endDate == null) {
+                JOptionPane.showMessageDialog(appView, "End date is invalid.", "", JOptionPane.WARNING_MESSAGE);
+            }
+            // make sure end date is after start date
+            else if (!endDate.after(startDate)) {
+                JOptionPane.showMessageDialog(appView, "End date is not after start date.", "",
+                        JOptionPane.WARNING_MESSAGE);
+            } else {
+                // finally get a chance to create league
+                int choice = JOptionPane.showConfirmDialog(appView, "Do you want to create this league?", "",
+                        JOptionPane.OK_CANCEL_OPTION);
+                if (choice == JOptionPane.OK_OPTION) {
+                    String name = createLeaguePane.getLeagueName();
+                    int capacity = createLeaguePane.getCapacity();
+                    Difficulty difficulty = createLeaguePane.getDifficulty();
+                    boolean isCreateLeague = LeagueUtility.createLeague(name, capacity, startDate, endDate,
+                            difficulty, mainUserModel.getUsername());
+                    
+                    boolean isAssignStock = LeagueUtility.assignStocks(createLeaguePane.getLeagueName(),
+                            createLeaguePane.getStocks());
+                    
+                    if (isCreateLeague && isAssignStock) {
+                        JOptionPane.showMessageDialog(appView, "The league was successfully created.", "",
+                                JOptionPane.INFORMATION_MESSAGE);
+                        createLeaguePane.resetFields();
+                        leagueListModel.allLeagues();
+                        appView.viewHome();
+                    } else {
+                        JOptionPane.showMessageDialog(appView, "The league was NOT successfully created.", "",
+                                JOptionPane.WARNING_MESSAGE);
+                    }
+                }
+            }
+        }
+        
     }
 }
